be9f2d2fa637ef20fd6fd6f3bb502339b2794bbe
antoninLallemand <antonin.lallemand@gmail.com>
2023-12-30 12:00:51 +0100

    simple circular buffer library, V1

---
+++ CircularBufferAL/CircularBufferAL.cpp
@@ -0,0 +1,26 @@
+#include "CircularBufferAL.h"
+
+
+void CircularBufferAL :: begin(uint16_t bufferSize){
+    CircularBufferSize = bufferSize;    //set buffer size
+}
+
+void CircularBufferAL :: writeData(int32_t data) {
+    circularBuffer[writeIndex] = data;  //add an element
+    if(writeIndex == CircularBufferSize-1) IndexScale = true; //when writing cursor come back to 0
+    writeIndex = (writeIndex + 1) % CircularBufferSize; //increment write cursor for next data
+    if(IndexScale && writeIndex > readIndex) IndexScale = false; //when writing cursor overtakes reading cursor
+}
+
+int32_t CircularBufferAL :: readData() {
+    int32_t data = circularBuffer[readIndex];   //read oldest unread element
+    if(readIndex == CircularBufferSize-1) IndexScale = false; //when reading cursor come back to 0
+    if(readIndex < writeIndex || IndexScale) //increment only if buffer is'nt empty
+        readIndex = (readIndex + 1) % CircularBufferSize;   //increment read cursor for next reading
+    return data;
+}
+
+bool CircularBufferAL :: isEmpty(){
+    return readIndex >= writeIndex && !IndexScale; //if reading and writing cursor are equal in the same round
+}
+
---
+++ CircularBufferAL/CircularBufferAL.h
@@ -0,0 +1,45 @@
+// Library managing a circular buffer
+// Contain 4 public functions
+// Author : Antonin Lallemand (2023)
+
+#ifndef CircularBufferAL_h
+#define CircularBufferAL_h
+
+#include <stdint.h>
+
+const int bufferSize = 100;
+
+class CircularBufferAL {
+public :
+
+    /*!
+    * \brief function to be implemented in the setup loop of the main code
+    * \param bufferSize maximum number of elements in the buffer (default value = 100)
+    */
+    void begin(uint16_t bufferSize);
+
+    /*!
+    * \brief adding an element in the buffer
+    * \param data data to add in the buffer
+    */
+    void writeData(int32_t data);
+
+    /*!
+    * \brief return the oldest unread value
+    */
+    int32_t readData();
+
+    /*!
+    * \brief return true if the buffer is empty
+    */
+    bool isEmpty();
+
+private :
+  int32_t circularBuffer[bufferSize];
+  uint16_t writeIndex = 0;
+  uint16_t readIndex = 0;
+  uint16_t CircularBufferSize = 100; //default value
+  bool IndexScale = false; //true if writeIndex has one round advance on readIndex
+};
+
+#endif
